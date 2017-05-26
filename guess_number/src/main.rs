e15bdc13fbdabf64780c103fdbeb7713322663f2
ShirleyChung <shirley@mail.kway.com.tw>
2017-05-26 18:26:48 +0800

    learn some basics

--- guess_number/src/main.rs
+++ guess_number/src/main.rs
@@ -1,3 +1,15 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Welcome! Guess number");
+    
+    println!("Come on! enter a number:");
+    
+    let mut guess = String::new(); // mutable string
+    
+    io::stdin().read_line(&mut guess)
+        .expect("Failed to read line.");
+    
+    println!("You guess: {}", guess);
+
 }
