8a8a21e6b922bf7859624758a734b68397d61e66
ShirleyChung <shirley@mail.kway.com.tw>
2017-05-26 19:09:03 +0800

    完成猜數字遊戲

--- guess_number/src/main.rs
+++ guess_number/src/main.rs
@@ -1,15 +1,44 @@
+extern crate rand;
+
 use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
 
 fn main() {
+    let sn = 1;
+    let en = 9;
     println!("Welcome! Guess number");
     
-    println!("Come on! enter a number:");
+    let myans = rand::thread_rng().gen_range(sn, en);
     
-    let mut guess = String::new(); // mutable string
+    loop {
+        println!("Come on! enter a number between({}~{})", sn, en);
+        
+        let mut guess = String::new(); // mutable string
+        
+        io::stdin().read_line(&mut guess)
+            .expect("Failed to read line.");
+        
+        println!("You guess: {}", guess);
+
+        //Parse unsigned integer
+        let guess: u32 = match guess.trim().parse() 
+        {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+//          .expect("Please enter a number..");
+        
+        match guess.cmp(&myans){
+            Ordering::Less      => println!("Too small!"),
+            Ordering::Greater   => println!("Too big!"),
+            Ordering::Equal      => {
+                println!("Yes you got it!");
+                break;
+            }
+        }
+    }
     
-    io::stdin().read_line(&mut guess)
-        .expect("Failed to read line.");
-    
-    println!("You guess: {}", guess);
+    println!("My secret number is {}", myans);
 
 }
