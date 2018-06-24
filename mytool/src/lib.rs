36deeb3fa20c1b530740d4703a1d13456dcff6bc
ShirleyChung <jonesr_tw@yahoo.com.tw>
2018-06-24 08:00:59 +0800

    write fabonicii number

--- mytool/src/lib.rs
+++ mytool/src/lib.rs
@@ -1,18 +1,25 @@
-#[cfg(mytools)]
-
-mod mytools {
-    #[mytools]
-    fn it_works() {
+pub fn fabonicii(n:i32)->i32{
+    if n <=0 {
+        0
+    }else if n == 1{
+        1
+    }else{
+        fabonicii(n-1) + fabonicii(n-2)
     }
+}
 
-    fn fabonicii(n:i32)->i32{
-        if n <= 0 {
-            0;
-        }
-        else if n == 1{
-            1;
-        }else{
-            fabonicii(n-1) + fabonicii(n-2);
-        }
+#[cfg(test)]
+mod tests {
+    use super::*;
+    
+    #[test]
+    fn it_works() {
+        assert_eq!(fabonicii(0), 0);
+        assert_eq!(fabonicii(1), 1);
+        assert_eq!(fabonicii(2), 1);
+        assert_eq!(fabonicii(3), 2);
+        assert_eq!(fabonicii(4), 3);
+        assert_eq!(fabonicii(5), 5);
+        assert_eq!(fabonicii(6), 8);
     }
 }
