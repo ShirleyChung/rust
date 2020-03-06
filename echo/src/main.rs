7906547957e0b17742cf9ceae81cd89c1d779e0a
鐘妘甄 <shirley@mail.kway.com.tw>
2020-03-06 23:48:25 +0800

    modify for running show client message

--- echo/src/main.rs
+++ echo/src/main.rs
@@ -1,5 +1,7 @@
 use std::net::{TcpListener, TcpStream};
 use std::io;
+use std::io::BufReader;
+use std::io::prelude::*;
 use std::env;
 
 /// 一個Client的角色
@@ -13,10 +15,11 @@ impl<'a> Client<'a> {
 		Client { ip: ip }
 	}
 	fn connect(&self) -> io::Result<()> {
-		let _client = TcpStream::connect(self.ip)?;
+		let client = TcpStream::connect(self.ip)?;
+		println!("{:?} connected.", client);
 		Ok(())
 	}
-	fn close() -> io::Result<()> {
+	fn close(&self) -> io::Result<()> {
 		Ok(())
 	}
 }
@@ -33,12 +36,28 @@ impl Server {
 		Server { port: p }
 	}
 	/// 處理進來的client
-	fn handle_client(_stream: TcpStream) -> std::io::Result<()> {
+	fn handle_client(stream: TcpStream) -> std::io::Result<()> {
+		println!("client {:?} connected!", stream);
+		let mut reader = BufReader::new(&stream);
+		let mut line   = String::new();
+		while line != "exit" {
+			line.clear();
+			println!("waiting client input...");
+			let _len = reader.read_line(&mut line)?;
+			if line.ends_with('\n') {
+				line.pop();
+			}
+			if line.ends_with('\r') {
+				line.pop();
+			}
+			println!("client: {}", line);
+		};
 		Ok(())
 	}
 	/// 開始監聽
 	fn listen(&self) -> io::Result<()> {
 		let url = format!("127.0.0.1:{}", self.port);
+		println!("begin listening {}", url);
 		let listener = TcpListener::bind(url)?;
 		for stream in listener.incoming() {
 			Server::handle_client(stream?)?;
@@ -52,15 +71,15 @@ fn main() -> io::Result<()> {
 
 	let args: Vec<String> = env::args().collect();
 	
-	if args.len() > 2 {
-		let target = &args[1];
+	if args.len() > 1 {
+		let target = &args[0];
 		if let Some(_c) = target.find(':') {
 			let echo_cli = Client::new(&target);
 			println!("connect to {}", target);
 			echo_cli.connect()?;
+			echo_cli.close()?;
 		} else if let Ok(p) = args[1].parse() {
 			let echo_svr = Server::new(p);
-			println!("listening on {}", p);
 		    echo_svr.listen()?;
 		}
 	} else {
