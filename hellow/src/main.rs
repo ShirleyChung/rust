824fb66e88d29849266f106b93edde6edbc4ce27
ShirleyChung <jonesr_tw@yahoo.com.tw>
2020-04-09 21:39:46 +0800

    add change

--- hellow/src/main.rs
+++ hellow/src/main.rs
@@ -1,3 +1,4 @@
+/* 展示lifetime的用法 */
 fn long<'a>(a:&'a str, b:&'a str) -> &'a str {
 	if a.len() > b.len() {
 		a
@@ -6,8 +7,13 @@ fn long<'a>(a:&'a str, b:&'a str) -> &'a str {
 	}
 }
 
+/* msg1的生命週期與msg2不同 */
+/* 所以函式中，要指明以msg1的生命週期為主 */
 fn main() {
 	let msg1 = String::from("123555");
+	for (i, c) in msg1.chars().enumerate() {
+		println!("{} = {}", i, c);
+	}
 	let longerstr;
 	{
 		let msg2 = String::from("1234");
