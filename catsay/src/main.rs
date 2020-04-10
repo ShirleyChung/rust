99ff170cde855d3a7f3bc50d9ddb36b72f56927e
ShirleyChung <jonesr_tw@yahoo.com.tw>
2020-04-10 22:33:38 +0800

    cat say add -d switch

--- catsay/src/main.rs
+++ catsay/src/main.rs
@@ -4,20 +4,23 @@ use structopt::StructOpt;
 struct Options {
 #[structopt(default_value="I am rust meow~")]
 /// what does cat say?
-	message: String
+	message: String,
+#[structopt(short="d", long="dead")]
+/// make cat appear dead
+	dead: bool,
 }
 
-fn say_hello(msg: &String) {
+fn say_hello(msg: &String, dead: bool) {
 	println!("{}", msg);
 	println!(" \\");
 	println!("  \\");
 	println!("     /\\_/\\");
-	println!("    ( o o )");
+	let eye = if dead { 'x' } else { 'o' };
+	println!("    ( {0} {0} )", eye);
 	println!("    =( I )=");
 }
 
 fn main() {
 	let options = Options::from_args();
-	let message = options.message;
-	say_hello(&message);
+	say_hello(&options.message, options.dead);
 }
