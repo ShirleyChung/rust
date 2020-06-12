7e9028963c35ef76d5f82a9028e1a245f2286658
鐘妘甄 <shirley@mail.kway.com.tw>
2020-06-12 14:21:10 +0800

    [sor_logparser] add some comments
    why: struct usage.
    how:
    
    note:

--- sor_logparser/src/main.rs
+++ sor_logparser/src/main.rs
@@ -9,16 +9,20 @@ struct Options {
 	filepath: String // Log檔路徑
 }
 
+/// 儲存解析後的陣列
 struct Parser {
 	providers: Vec<String>,
 }
 
+/// 陣列的操作函式
 impl Parser{
 	fn parse_line(&mut self, line: &str) {
 		self.providers.push(line.to_string());
 	}
 }
 
+/// 第一參數指定檔案
+/// 將其讀入陣列似便解析
 fn main() -> Result<()> {
 	let options    = Options::from_args();
 
