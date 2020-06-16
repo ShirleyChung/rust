0ea74887409b967e521608cc2ad14a0fb7987edc
鐘妘甄 <shirley@mail.kway.com.tw>
2020-06-16 17:13:57 +0800

    [sor_logparser] parse to array
    why:
    how:
    
    note:

--- sor_logparser/src/main.rs
+++ sor_logparser/src/main.rs
@@ -3,6 +3,7 @@ use std::io::*;
 use std::io::{BufRead, BufReader};
 //use std::io::prelude::*;
 use std::fs::File;
+use std::collections::HashMap;
 
 #[derive(StructOpt)]
 struct Options {
@@ -12,10 +13,19 @@ struct Options {
 /// 儲存解析後的陣列
 struct Parser {
 	providers: Vec<String>,
+	reqRecs: HashMap<String, Vec<String>>,
+	ordRecs: HashMap<String, Vec<String>>,
 }
 
 /// 陣列的操作函式
 impl Parser{
+	fn new()->Parser {
+		Parser{ 
+			providers: Vec::<String>::new(),
+			reqRecs: HashMap::<String, Vec<String>>::new(),
+			ordRecs: HashMap::<String, Vec<String>>::new() 
+			}
+	}
 	fn parse_line(&mut self, line: &str) {
 		self.providers.push(line.to_string());
 	}
@@ -28,7 +38,7 @@ fn main() -> Result<()> {
 
 	let f           = File::open(options.filepath)?;
 	let reader     = BufReader::new(f);
-	let mut parser = Parser{ providers: Vec::<String>::new() };
+	let mut parser = Parser::new();
 	
 	for line in reader.lines() {
 		match line {
@@ -36,5 +46,15 @@ fn main() -> Result<()> {
 			_ => continue,
 		}
 	}
+	
+	for line in parser.providers {
+		let toks = line.split('\x01');
+		for (i, tok) in toks.enumerate() {
+			println!("{}, {:?}", i, tok);
+		}
+		
+	}
+	
 	Ok(())
 }
+
