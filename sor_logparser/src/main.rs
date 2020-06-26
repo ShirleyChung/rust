587d1de177b693f8539a6315c68889a2844f5b23
ShirleyChung <jonesr_tw@yahoo.com.tw>
2020-06-26 10:51:20 +0800

    [sor_logparser] reconstruct data storage, to search ords

--- sor_logparser/src/main.rs
+++ sor_logparser/src/main.rs
@@ -4,6 +4,7 @@ use std::io::{BufRead, BufReader};
 //use std::io::prelude::*;
 use std::fs::File;
 use std::collections::HashMap;
+use std::collections::LinkedList;
 use std::fmt;
 
 /// 第一步，程式的參數接收
@@ -18,47 +19,103 @@ struct Options {
 
 /// 第二步，定義儲存的資料結構
 struct Rec {
-	recs_vec: Vec<String>,
+	reqs_vec: Vec<String>,
 	line    : String,
 	log     : String,
 }
 
-struct ReqData {
-	tables: HashMap<String, HashMap<String, usize>>,
-	recs:   HashMap<String, Rec>,
+struct TableRec {
+	index: HashMap<String, usize>,
+	recs : Vec<String>,
 }
 
-impl ReqData {
-	fn new() -> ReqData {
-		ReqData {
-			tables: HashMap::<String, HashMap<String, usize>>::new(),
-			recs  : HashMap::<String, Rec>::new(),
+impl TableRec {
+	fn new() -> TableRec {
+		TableRec {
+			index: HashMap::<String, usize>::new(),
+			recs : Vec::<String>::new(),
 		}
 	}
-	fn insert_data(&mut self, toks: &Vec<String>, line: &str ) {
-		let key = &toks[1];		
+}
+
+type TableRecMap = HashMap<String, TableRec>;
+type ReqRecMap   = HashMap<String, Rec>;
+type OrdRecMap   = HashMap<String, LinkedList<Rec>>;
+
+struct OrderRec {
+	tables: TableRecMap,
+	reqs:   ReqRecMap,
+	ords:   OrdRecMap,
+}
+
+impl OrderRec {
+	fn new() -> OrderRec {
+		OrderRec {
+			tables: TableRecMap::new(), // 表格名-欄位-index
+			reqs  : ReqRecMap::new(),   // reqKey-一筆Req
+			ords  : OrdRecMap::new(),   // ordKey-一筆Ord
+		}
+	}
+	fn insert_rec(&mut self, toks: Vec<String>, line: &str ) {
+		let key = &toks[1];
+		let hdr = &toks[0];		
 		if key == "-" { // 沒有key值的，是表格
 			// Provider table的名稱, 例如 TwsNew
 			let table_name = (&toks[2]).to_string();
 			if !self.tables.contains_key(&table_name) {
 				// 建立Provider的陣列
-				let mut fields = HashMap::<String, usize>::new();
+				let mut tablerec = TableRec::new();
 				// 插入每個Provider的Field
 				for (idx, name) in toks.iter().enumerate() {
-					fields.insert(name.to_string(), idx);
+					tablerec.index.insert(name.to_string(), idx);
 				}
-				self.tables.insert(table_name, fields);
+				tablerec.recs = toks.to_vec();
+				self.tables.insert(table_name, tablerec);
 			}
 		}
-		else {  // 依key將記錄儲存到hashmap中
-			self.recs.insert((&toks[1]).to_string(), Rec{recs_vec: toks.to_vec(), line: line.to_string(), log: String::new()});
+		else if "Req" == hdr {  // 依key將記錄儲存到hashmap中
+			self.reqs.insert(key.to_string(), Rec{reqs_vec: toks.to_vec(), line: line.to_string(), log: String::new()});
+		}
+		else if "Ord" == hdr {	
+			let rec = Rec{reqs_vec: toks.to_vec(), line: line.to_string(), log: String::new()};
+			self.ords.entry(key.to_string()).or_insert(LinkedList::<Rec>::new()).push_front(rec);
+		}
+		else {
+			unimplemented!();
 		}
 	}
-	/// 以index, 找出rec中相等於target的rec
-	fn find_rec(&self, recs: &HashMap<String, Rec>, key_index: usize, target: &str) {
-		for (key, rec) in recs {
-			if rec.recs_vec[key_index] == target.to_string() {
-				println!("found key = {}, {}, {}", key, rec.line, rec.log)
+	/// 以index, 找出ords中相等於target的rec
+	fn find_req(&self, table_name: &str, key_index: usize, target: &str) {
+		for (key, rec) in &self.reqs  {
+			if  rec.reqs_vec.len() < 3 || rec.reqs_vec[2] != table_name { // 表格名不同則跳過
+				continue;
+			}
+			if rec.reqs_vec.len() > key_index {
+				if rec.reqs_vec[key_index] == target.to_string() {
+					println!("found key = {}, {}, {}", key, rec.line, rec.log)
+				}
+			} else {
+				println!("line:{}, \n fields miss match.", rec.line);
+			}
+		}
+	}
+	/// 以index, 找出reqs中相等於target的rec
+	fn find_ord(&self, table_name: &str, key_index: usize, target: &str) {
+		for (key, list) in &self.ords  {
+			match list.front() {
+				Some(rec) => {
+					if  rec.reqs_vec.len() < 3 || rec.reqs_vec[2] != table_name { // 表格名不同則跳過
+						continue;
+					}
+					if rec.reqs_vec.len() > key_index {
+						if rec.reqs_vec[key_index] == target.to_string() {
+							println!("found key = {}, {}, {}", key, rec.line, rec.log)
+						}
+					} else {
+						println!("line:{}, \n fields miss match.", rec.line);
+					}
+				},
+				_=>println!("{} is empty", key),
 			}
 		}
 	}
@@ -66,8 +123,18 @@ impl ReqData {
 	fn check_req_data(&self, table_name: &str, field_name: &str, search_target: &str) {
 		match self.tables.get(table_name) {
 			Some(table) => { // 表中，該欄位的index
-				match table.get(field_name){
-					Some(idx) => self.find_rec(&self.recs, *idx, search_target),
+				match table.index.get(field_name){
+					Some(idx) => { 
+						if table.recs[0] == "Req" {
+							self.find_req(table_name, *idx, search_target);
+						}
+						else if table.recs[0] == "Ord" {
+							self.find_ord(table_name, *idx, search_target);							
+						}
+						else {
+							unimplemented!();
+						}
+					},
 					_=> println!("{} doesn't exist", field_name),
 				}
 			},
@@ -78,16 +145,14 @@ impl ReqData {
 
 /// 儲存解析後的陣列
 struct Parser {
-	req_recs :ReqData,
-	ord_recs :ReqData, 
+	ord_recs :OrderRec,
 }
 
 /// 陣列的操作函式
 impl Parser {
 	fn new()->Parser {
 		Parser{ 
-			req_recs: ReqData::new(),
-			ord_recs: ReqData::new(), 
+			ord_recs: OrderRec::new(),
 			}
 	}
 
@@ -95,20 +160,13 @@ impl Parser {
 	fn parse_line(&mut self, line: &str) {
 		let toks : Vec<String> = line.to_string().split('\x01').map(|s| s.to_string()).collect();
 		if toks.len() > 3 {
-			if toks[0] == "Req" {
-				self.req_recs.insert_data(&toks, line);
-			}
-			else if toks[0] == "Ord" {
-				self.ord_recs.insert_data(&toks, line);
-			}
+			self.ord_recs.insert_rec(toks, line);
 		}
 	}
 	
 	/// 輸入 表名/欄位名/值 來尋找目標
 	fn find_by_field(&mut self, table_name: &str, field_name: &str, search_target: &str) {
 		// 先找看看 Req表
-		self.req_recs.check_req_data(table_name, field_name, search_target);
-		// 再找看看 Ord表
 		self.ord_recs.check_req_data(table_name, field_name, search_target);
 	}
 }
@@ -116,9 +174,9 @@ impl Parser {
 /// 使Parse類別能以println列印出來
 impl fmt::Display for Parser {
 	fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-		write!(f, "req prividers: {}, ord providers:{}, reqs: {}, ords: {}", 
-			self.req_recs.tables.len(), self.ord_recs.tables.len(), 
-			self.req_recs.recs.len(),   self.ord_recs.recs.len())
+		write!(f, "req prividers: {}, reqs: {}", 
+			self.ord_recs.tables.len(), 
+			self.ord_recs.reqs.len())
 	}
 }
 
@@ -127,7 +185,7 @@ impl fmt::Display for Parser {
 fn main() -> Result<()> {
 	let options    = Options::from_args();
 
-	let f           = File::open(options.filepath)?;
+	let f          = File::open(options.filepath)?;
 	let reader     = BufReader::new(f);
 	let mut parser = Parser::new();
 	
